e2701ebab0c7dd0f76c9a94f9dfa8e8b7b1c0441
Robert Tomlinson <270105+robtomlinson@users.noreply.github.com>
2018-10-24 14:32:49 -0500

    Added TestSuperWorker

---
+++ src/test/java/testthreesolid/TestSuperWorker.java
@@ -0,0 +1,24 @@
+package threesolid;
+
+import org.junit.jupiter.api.BeforeAll;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class TestSuperWorker {
+
+   private  SuperWorker w = new SuperWorker();
+
+   @Test
+   @DisplayName("SuperWorker Test")
+   public void testSuperWorker()
+   {
+      assertEquals(w.eat(), true);
+      assertEquals(w.sick(), true);
+      assertEquals(w.work(), true);
+   }
+
+}
