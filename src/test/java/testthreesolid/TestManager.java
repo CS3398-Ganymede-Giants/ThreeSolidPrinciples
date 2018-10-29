2647a140865125b94babcda17c3b9ff3f84fd155
Tyler Hooks <tylerhooks@Tylers-Mac-mini.local>
2018-10-28 20:40:13 -0500

    change

--- src/test/java/testthreesolid/TestManager.java
+++ src/test/java/testthreesolid/TestManager.java
@@ -11,14 +11,14 @@ public class TestManager {
     private Manager m = new Manager();
 
     @Test
-    @DisplayName("Manager Test")
+    @DisplayName("<T-hooks>  Manager Test")
     public void testManager()
     {
       assertEquals(m.manage(),true);
     }
 
     @Test
-    @DisplayName("Manager Fail Test")
+    @DisplayName("<T-hooks> Manager Fail Test")
     public void testFailManager(){
       try{
           assertEquals(m.manage(),false);
