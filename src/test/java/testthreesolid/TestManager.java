8bd15f7cb1bbf0187884984fa6a9ac9bc59a2758
Tyler Hooks <tylerhooks@Tylers-Mac-mini.local>
2018-10-28 20:11:15 -0500

    TestManager file

---
+++ src/test/java/testthreesolid/TestManager.java
@@ -0,0 +1,32 @@
+package threesolid;
+
+import org.junit.jupiter.api.BeforeAll;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Manager{
+    private Manager m = new Manager();
+
+    @Test
+    @DisplayName("Manager Test")
+    public void testManager()
+    {
+      assertEquals(m.manage(),true);
+    }
+
+    @Test
+    @DisplayName("Manager Fail Test")
+    public void testFailManager(){
+      try{
+          assertEquals(m.manage(),false);
+          fail("Manager did not return false");
+      }
+      catch (org.opentest4j.AssertionFailedError e) {
+          System.out.format("\tManager Fail  ... \n");
+      }
+   }
+
+}
